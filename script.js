882d28c1eaed91edb3ea3fd6a3ba012e3fb4a192
mxprs <seriousgrowthtalk@gmail.com>
2022-01-18 17:20:31 +0100

    finished

--- script.js
+++ script.js
@@ -6,68 +6,82 @@ const gameChoice = ["Rock", "Paper", "Scissor"];
 let scoreComputer = 0;
 let scoreUser = 0;
 
-let computerChoice = "";
-let userChoice = "";
-
 function computerPlay() {
     let computerChoiceInt = Math.floor(Math.random() * 3);
     return gameChoice[computerChoiceInt];
 }
 
 function userPlay() {
-    let userChoiceNbr = prompt("Enter a choice number - Rock (0), Paper (1) or Scissor(2)?");
+    let userChoiceNbr = prompt("Enter a choice number - Rock (0), Paper (1) or Scissor(2): ");
     return gameChoice[userChoiceNbr];
 }
 
-function calculateScore() {
+function gameLogic() {
     // Selection of move
-    computerChoice = computerPlay();
-    userChoice = userPlay();
+    let computerChoice = computerPlay();
+    let userChoice = userPlay();
 
     let result = computerChoice + userChoice;
 
+    //set logic of the game and who won through result variable
     switch (result) {
         case "ScissorRock":
-            scoreUser++;
-            console.log("User win!");
-            break;
         case "PaperScissor":
-            scoreUser++;
-            console.log("User win!");
-            break;
         case "RockPaper":
             scoreUser++;
             console.log("User win!");
             break;
         case "ScissorPaper":
-            scoreComputer++;
-            console.log("Computer win!");
-            break;
         case "RockScissor":
-            scoreComputer++;
-            console.log("Computer win!");
-            break;
         case "PaperRock":
             scoreComputer++;
             console.log("Computer win!");
             break;
         case "PaperPaper":
-            console.log("It's a draw...");
-            break;
         case "ScissorScissor":
-            console.log("It's a draw...");
-            break;
         case "RockRock":
             console.log("It's a draw...");
+            break;
+        default:
+            console.log("Error value in choices: [result == ??]");
             break;
     }
 
     // Debugg
-    console.log(result);
+    console.log(`[USER]${userChoice} | [COMPUTER]${computerChoice}`);
 }
 
 
-calculateScore();
+function calculateScore(nbrGame) {
+    // while score max is not achieved then keep playing
+    while (nbrGame > (scoreComputer + scoreUser)) {
+        console.log(`[USER] Score = ${scoreUser} - [COMPUTER] Score = ${scoreComputer}`)
+        gameLogic();
+    }
+
+    // Announce winner
+    if (scoreComputer > scoreUser) {
+        console.log("You lost against the computer...");
+    } else if (scoreComputer < scoreUser) {
+        console.log("You WON!!");
+    } else {
+        console.log("It's a draw....")
+    }
+}
+
+
+function startGame(nbrGame) {
+    console.log("Game starts....");
+
+    calculateScore(nbrGame);
+
+    console.log(`[USER] Score = ${scoreUser} - [COMPUTER] Score = ${scoreComputer}`)
+
+    console.log("Game finished!");
+
+}
+
+startGame(5);
 
 // set the choices
 // set the scores for user and computer
